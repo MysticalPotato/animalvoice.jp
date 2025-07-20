933790ecf83acac934528bde33b7498b8bdf6a96
Mark Walters <m_walters92@hotmail.com>
2025-07-20 13:10:34 +0900

    add headers to margin reset

--- views/partials/email_template_newsletter.php
+++ views/partials/email_template_newsletter.php
@@ -23,7 +23,7 @@
 
     <style>
         /* CLIENT-SAFE RESET */
-        body, table, td, p {
+        body, table, td, p, h1, h2, h3 {
             margin: 0;
             padding: 0;
             font-family: sans-serif;
